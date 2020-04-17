ab6ef1c34654189677b8275ecad666e71cad8880
Your Name <renatakersyte123@gmail.com>
2020-04-17 13:22:05 +0300

    antra

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 "use strict";
 
+var kaina;
+var miestas;
+var pristatymokaina = 20;
+var pristatymoriba = 50;
+var pristatymoporeikis;
+
+do{
+kaina = prompt("Įveskite kainą");
+kaina = Number(kaina);
+}
+while (isNaN(kaina) || kaina <= 0);
+
+var pristymoporeikis = confirm("Ar reikalingas pristatymas?");
+if (confirm){
+    do {
+        miestas = prompt("Pristatymo miestas?");
+        }
+    while (miestas.length ===0);
+
+
+    if ((kaina >= pristatymoriba)||(miestas==="Vilnius")) {
+        console.log ('kaina:' +kaina)
+        console.log ('Prekę nemokamai pristatysime į '+miestas+' per 1-3 dienas.');
+    }
+
+    else {
+        var total = kaina + pristatymokaina;
+
+        console.log('kaina:' +kaina);
+        console.log('pristatymokaina'+pristatymokaina);
+        console.log('total:' +total);
+        console.log('Prekę pristatysime į '+miestas+' per 1-3 dienas.');
+    }
+}
+
+else {
+    console.log('kaina:' + kaina);
+    console.log('Prekes galite atsiimti nemokamai Vilniuje adresu Gedimino pr. 1a');
+}
+
+
+/*
 var tax = 0.21;
 var price;
 
@@ -16,3 +58,4 @@ var minustax = price - tax;
 console.log('goods price with tax: ' + total);
 console.log('tax: ' + tax);
 console.log('price minus tax: ' + minustax);
+*/
